2ac61f8e0b1911f683941afa8fb47d4502a8b640
nguyenphuong152 <17520927@gm.uit.edu.vn>
2020-11-24 09:23:34 +0700

    del folder frontend-cus frontend-admin

--- frontend-customer/.history/src/views/Account_20201119164123.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="account">
-    <v-sheet color="grey" p-32>
-      <a class="slogan-3">My Account</a>
-      <br>
-      <a class="title-shoes">Home/My Account</a>
-    </v-sheet>
-  </div>
-</template>
--- frontend-customer/src/views/Cart.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="cart">
-    <h1>This is cart</h1>
-  </div>
-</template>
